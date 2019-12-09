8159f1b053f66482d716596f08db252b001e1ac1
jmoh3 <jmoh3@illinois.edu>
2019-12-09 15:57:57 -0600

    Finished python floyd warshalls implementation

--- floyd_warshalls.py
+++ floyd_warshalls.py
@@ -1,32 +1,35 @@
-vertices = [ (3, 3),
-(8, 3),
-(14, 3),
-(21, 3),
+import math
+
+vertices = [
+(3, 3),
+(11, 3),
+(18, 3),
 (25, 3),
-(11, 6),
-(18, 6),
+
 (3, 8),
-(14, 8),
+(11, 8),
+(18, 8),
 (25, 8),
-(9, 11),
-(20, 11),
-(3, 14),
-(25, 14),
-(9, 15),
-(20, 15),
-(3, 18),
-(9, 18),
-(19, 18),
-(25, 18),
-(3, 22),
-(12, 22),
-(17, 22),
-(25, 22),
+
+(3, 12),
+(8, 12),
+(21, 12),
+(25, 12),
+
+(3, 17),
+(8, 17),
+(21, 17),
+(25, 17),
+
+(3, 21),
+(11, 21),
+(18, 21),
+(25, 21),
+
 (3, 26),
-(7, 26),
-(15, 26),
-(20, 26),
-(25, 26),
+(11, 26),
+(18, 26),
+(25, 26)
 ]
 
 edges = [ (0, 1),
@@ -89,14 +92,61 @@ edges = [ (0, 1),
 #                dist[i][j] ← dist[i][k] + dist[k][j]
 #                next[i][j] ← next[i][k]
 
-# procedure Path(u, v)
-#    if next[u][v] = null then
-#        return []
-#    path = [u]
-#    while u ≠ v
-#        u ← next[u][v]
-#        path.append(u)
-#    return path
+dist = [[math.inf for x in range(len(vertices))] for y in range(len(vertices))]
+next = [[-1 for x in range(len(vertices))] for y in range(len(vertices))]
 
 for edge in edges:
-    print(f'.half {edge[0]}, {edge[1]}')
+    u = vertices[edge[0]]
+    v = vertices[edge[1]]
+
+    distance = math.sqrt((u[0] - v[0])**2 + (u[1] - v[1])**2)
+
+    dist[edge[0]][edge[1]] = distance
+    next[edge[0]][edge[1]] = edge[1]
+
+    dist[edge[1]][edge[0]] = distance
+    next[edge[1]][edge[0]] = edge[0]
+
+for i in range(len(vertices)):
+    dist[i][i] = 0
+    next[i][i] = i
+
+for k in range(len(vertices)):
+    for i in range(len(vertices)):
+        for j in range(len(vertices)):
+            if dist[i][j] > dist[i][k] + dist[k][j]:
+                dist[i][j] = dist[i][k] + dist[k][j]
+                next[i][j] = next[i][k]
+            if dist[j][i] > dist[j][k] + dist[k][i]:
+                dist[j][i] = dist[j][k] + dist[k][i]
+                next[j][i] = next[k][i]
+
+# print(next)
+new_next = [[[next[u][v], 0, dist[u][v]] for v in range(len(vertices))] for u in range(len(vertices))]
+
+for i in range(len(vertices)):
+    for j in range(len(vertices)):
+        if i == j:
+            continue
+
+        u = vertices[i]
+        v = vertices[j]
+        b = vertices[next[i][j]]
+
+        # find angle between u and b
+        angle = math.atan2(b[1] - u[1], b[0] - u[0])
+
+        # find distance from u to b
+        distance = math.sqrt((b[0] - u[0])**2 + (b[1] - u[1])**2)
+
+        new_next[i][j][1] = int(angle)
+        new_next[i][j][2] = int(distance)
+
+
+print('next:')
+for row in new_next:
+    string = '.half: '
+    for elem in row:
+        for thing in elem:
+            string += str(thing) + ', '
+    print(string[:-2])
